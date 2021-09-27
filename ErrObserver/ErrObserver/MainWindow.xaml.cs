12dbd1810cb1fc0e853397381bedb40084b695f7
Szymon Kamiński <szkaminski@tuchola.pl>
2021-09-27 10:58:56 +0200

    add application resources to GUI

--- ErrObserver/ErrObserver/MainWindow.xaml.cs
+++ ErrObserver/ErrObserver/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ErrObserver.Rgx;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,40 @@ namespace ErrObserver
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<string> sslOptionsList = new List<string> { "TAK", "NIE" };
+
         public MainWindow()
         {
             InitializeComponent();
+
+            this.sslOptions.ItemsSource = sslOptionsList;
+            this.sslOptions.SelectedIndex = 1;
+        }
+
+        private void emailAddr_KeyUp(object sender, KeyEventArgs e)
+        {
+            var result = default(bool);
+            var textBox = sender as TextBox;
+            var text = textBox.Text;
+
+            result = ValidationCl.checkEmailAddr(ref text);
+            if (result == false)
+                textBox.Foreground = Brushes.Red;
+            else
+                textBox.Foreground = Brushes.Blue;
+        }
+
+        private void btnController(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            var name = button.Name;
+            switch(name)
+            {
+                case "addBtn":
+                    var path = this.filepath.Text;
+                    this.filepathList.Items.Add(path);
+                    break;
+            }    
         }
     }
 }
---
+++ ErrObserver/ErrObserver/Validation/ValidationCl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Net.Mail;
+
+namespace ErrObserver.Rgx
+{
+    class ValidationCl
+    {
+        static public bool checkEmailAddr(ref string email)
+        {
+            var result = true;
+            try
+            {
+                MailAddress mailAddress = new MailAddress(email);
+            }
+            catch (FormatException err)
+            {
+                result = false;
+            }
+            return result;
+        }
+    }
+}
